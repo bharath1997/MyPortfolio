8bf499533672474e927d168d1b91a01953c5125d
bharathvenkatesan <bviperbharath4@gmail.com>
2024-06-21 13:38:22 +0530

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/About.kt
@@ -0,0 +1,13 @@
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * @author bharathvenkatesan
+ * Created 15/06/24 at 8:02 PM
+ */
+
+@Composable
+fun AboutMe(modifier: Modifier = Modifier) {
+
+}
